6b3ca75a865b32e749a2516c0171ce71777af1e7
WARABI\81801 <sato0807.forwork@gmail.com>
2023-12-20 18:56:08 +0900

    ユーザーの新規登録画面にバリデーションを追加

--- app/Http/Requests/Auth/RegisterFormRequest;.php
+++ app/Http/Requests/Auth/RegisterFormRequest;.php
@@ -13,7 +13,7 @@ class RegisterFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -31,11 +31,33 @@ class RegisterFormRequest extends FormRequest
             'under_name_kana' => 'required|string|/\A[ァ-ヿ]+\z/u|max:30',
             'mail_address' => 'required|email|unique:users,mail_address|max:100',
             'sex' => 'required|in:1,2,3',
-            'old_year' => 'required|',
-            'old_month' => '',
-            'old_day' => '',
+            'old' => 'date'
+            'old_year' => 'required_with:old_month,old_day',
+            'old_month' => 'required_with:old_year,old_day',
+            'old_day' => 'required_with:old_year,old_month',
             'role' => 'required|in:1,2,3,4',
             'password' => 'required|min:8|max:30|same:password_confirmation'
         ];
     }
+
+    public function getValidatorInstance()
+    {
+        if ($this->input('old_day') && $this->input('old_month') && $this->input('old_year'))
+        {
+            $old_time = implode('-', $this->only(['old_year', 'old_month', 'old_day']));
+            $this->merge([
+                'old' => $old_time
+            ]);
+        }
+        // もし ($this->取得できる('old_day') と $this->取得できる('old_month') と $this->取得できる('old_year'))ならば
+        // {
+        //     $old_time = 連結して取得する('-'で, $this->これだけ(['old_year', 'old_month', 'old_day']));
+        //     $this->データを追加する([
+        //         'old' に $old_time を
+        //     ]);
+        // }
+
+        return parent::getValidatorInstance();
+    }
+
 }
